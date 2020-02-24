6e1e6954a07b3f74d871a4c759ac295e699f035d
cassandra calescu <casscalescu@gmail.com>
2020-02-24 18:10:45 +1100

    routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+  
+  resources :users, except: [ :destroy, :index ]
+  resources :surfboards
+  resources :bookings, only: [ :new, :create, :index, :show ]
+
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
