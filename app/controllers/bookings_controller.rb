db30fd2a795a30b1b33759b74085a2138540cbe7
Lizhenan Li <lizhenanl54@gmail.com>
2020-02-26 14:21:18 +1100

    added total_price feature when creating a new booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -8,7 +8,7 @@ class BookingsController < ApplicationController
   def create
     @booking = Booking.new(booking_params)
     @booking.user = current_user
-    @booking.total_price = (@booking.end_date - @booking.start_date)
+    @booking.total_price = calculate_total_price(@booking)
     if @booking.save
       redirect_to bookings_path
     else
@@ -30,5 +30,15 @@ class BookingsController < ApplicationController
 
   def booking_params
     params.require(:booking).permit(:start_date, :end_date, :surfboard_id)
+  end
+
+  def calculate_total_price(booking)
+    surfboard = booking.surfboard
+    price = case surfboard.price_duration
+            when 'Week' then (((booking.end_date - booking.start_date) / 604_800) * surfboard.price).round(2)
+            when 'Day' then (((booking.end_date - booking.start_date) / 86_400) * surfboard.price).round(2)
+            when 'Hour' then (((booking.end_date - booking.start_date) / 3600) * surfboard.price).round(2)
+            end
+    price
   end
 end
