d890981b5316a92fdbe5490406d5f874c9650310
cassandra calescu <casscalescu@gmail.com>
2020-02-27 14:53:27 +1100

    ...

--- app/javascript/plugins/init_autocomplete.js
+++ app/javascript/plugins/init_autocomplete.js
@@ -4,7 +4,8 @@ export const initAutoCompleteField = (selector) => {
   var placesAutocomplete = places({
     appId: 'pl0S1BCHOV2L',
     apiKey: 'f6ee5094a791f6a364277c453e586b55',
-    container: document.querySelector('#surfboard_location')
+    container: document.querySelector('#surfboard_location'),
+    useDeviceLocation: true
   });
   placesAutocomplete.search();
   placesAutocomplete.on('change', e => console.log(e.suggestion));
