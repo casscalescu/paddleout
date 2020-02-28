8c8a4a7a59bfd6ce3acbec503455ec5f6b63bc46
Lizhenan Li <lizhenanl54@gmail.com>
2020-02-28 11:37:06 +1100

    date picker and date validations not fully functioned but should work somehow

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -12,6 +12,7 @@ class BookingsController < ApplicationController
     if @booking.save
       redirect_to bookings_path
     else
+      @surfboard = @booking.surfboard
       render 'bookings/new'
     end
   end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,9 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 import { initMapbox } from '../plugins/init_mapbox';
 import { initAutoCompleteField } from '../plugins/init_autocomplete';
+import 'flatpickr/dist/flatpickr.css';
+import { toggleDateInputs } from '../components/booking';
 
+toggleDateInputs();
 initMapbox();
 initAutoCompleteField('#surfboard_location');
